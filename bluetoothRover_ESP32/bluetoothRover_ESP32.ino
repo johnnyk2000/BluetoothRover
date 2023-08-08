7500950faf8bfab9c3c394401a0983fb894d28d1
SlimRunner <dv.polygon@gmail.com>
2023-08-08 13:41:03 -0700

    add second arm

--- bluetoothRover_ESP32/bluetoothRover_ESP32.ino
+++ bluetoothRover_ESP32/bluetoothRover_ESP32.ino
@@ -12,6 +12,7 @@
 BluetoothSerial SerialBT; // declares the Bluetooth object
 
 constexpr int LEFT_SERVO_PIN = 14;
+constexpr int RIGHT_SERVO_PIN = 11;
 
 constexpr int EN_A_PIN = 13;
 constexpr int IN_1_PIN = 27;
@@ -22,6 +23,7 @@ constexpr int EN_B_PIN = 12;
 
 Navigator mainAxle;
 Servo leftArm;
+Servo rightArm;
 
 void setup() {
   Serial.begin(115200);
@@ -35,6 +37,8 @@ void setup() {
 	ESP32PWM::allocateTimer(3);
 	leftArm.setPeriodHertz(50);    // standard 50 hz servo
 	leftArm.attach(LEFT_SERVO_PIN, 500, 2400);
+	rightArm.setPeriodHertz(50);    // standard 50 hz servo
+	rightArm.attach(RIGHT_SERVO_PIN, 500, 2400);
   
   mainAxle.begin(EN_A_PIN, IN_1_PIN, IN_2_PIN, IN_3_PIN, IN_4_PIN, EN_B_PIN);
 }
@@ -44,6 +48,7 @@ char readIn;
 void loop() {
   constexpr int angleDelta = 10;
   static int angleLeft = 0;
+  static int angleRight = 0;
 
   readIn = '\0';
   if (SerialBT.available()) // if there are bytes available in the buffer
@@ -69,6 +74,12 @@ void loop() {
   } else if (readIn == 'e') {
     angleLeft = min(angleLeft + angleDelta, 180);
     leftArm.write(angleLeft);
+  } else if (readIn == 'i') {
+    angleRight = max(angleRight - angleDelta, 0);
+    rightArm.write(angleRight);
+  } else if (readIn == 'p') {
+    angleRight = min(angleRight + angleDelta, 180);
+    leftArm.write(angleRight);
   }
   delay(20);
 }
