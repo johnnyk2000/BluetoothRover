a4ef4a7e1b203f73933a0218025b7ce24efdcc1f
SlimRunner <dv.polygon@gmail.com>
2023-08-08 15:29:51 -0700

    comment out right arm

--- bluetoothRover_ESP32/bluetoothRover_ESP32.ino
+++ bluetoothRover_ESP32/bluetoothRover_ESP32.ino
@@ -23,7 +23,7 @@ constexpr int EN_B_PIN = 12;
 
 Navigator mainAxle;
 Servo leftArm;
-Servo rightArm;
+// Servo rightArm;
 
 void setup() {
   Serial.begin(115200);
@@ -37,8 +37,8 @@ void setup() {
 	ESP32PWM::allocateTimer(3);
 	leftArm.setPeriodHertz(50);    // standard 50 hz servo
 	leftArm.attach(LEFT_SERVO_PIN, 500, 2400);
-	rightArm.setPeriodHertz(50);    // standard 50 hz servo
-	rightArm.attach(RIGHT_SERVO_PIN, 500, 2400);
+	// rightArm.setPeriodHertz(50);    // standard 50 hz servo
+	// rightArm.attach(RIGHT_SERVO_PIN, 500, 2400);
   
   mainAxle.begin(EN_A_PIN, IN_1_PIN, IN_2_PIN, IN_3_PIN, IN_4_PIN, EN_B_PIN);
 }
@@ -76,10 +76,10 @@ void loop() {
     leftArm.write(angleLeft);
   } else if (readIn == 'i') {
     angleRight = max(angleRight - angleDelta, 0);
-    rightArm.write(angleRight);
+    // rightArm.write(angleRight);
   } else if (readIn == 'p') {
     angleRight = min(angleRight + angleDelta, 180);
-    leftArm.write(angleRight);
+    // rightArm.write(angleRight);
   }
   delay(20);
 }
