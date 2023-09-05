6d5ff63c5b1dc7ffc1f020fc5e8d3f74e508ec0d
Dice-k5599 <108194726+Dice-k5599@users.noreply.github.com>
2023-09-04 21:17:22 -0700

    Update Ver2_Bluetooth_Rover_ESP32.ino
    Added code for UART connection so that LCD works + pin definition changes because I changed from using adafruit esp32 feather to esp32 wroom (dev module)

--- Ver2_bluetoothRover_ESP32/Ver2_Bluetooth_Rover_ESP32.ino
+++ Ver2_bluetoothRover_ESP32/Ver2_Bluetooth_Rover_ESP32.ino
@@ -3,63 +3,89 @@
 #include "BluetoothSerial.h"
 
 // for error catching bluetooth connection
-#if !defined(CONFIG_BT_ENABLED) ||                                             \
-    !defined(CONFIG_BLUEDROID_ENABLED)
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)           //error catching
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
 //Pin Number Constant Definitions
-const int SERVO_PIN = 14;
-const int MOTOR_EN_A_PIN = 13;
-const int MOTOR_IN_1_PIN = 12;
-const int MOTOR_IN_2_PIN = 27;
-const int MOTOR_IN_3_PIN = 33;
-const int MOTOR_IN_4_PIN = 15;
-const int MOTOR_EN_B_PIN = 32;
+const int LEFT_SERVO_PIN = 12;
+const int RIGHT_SERVO_PIN = 13;
+const int MOTOR_EN_A_PIN = 6;
+const int MOTOR_IN_1_PIN = 7;
+const int MOTOR_IN_2_PIN = 8;
+const int MOTOR_IN_3_PIN = 9;
+const int MOTOR_IN_4_PIN = 10;
+const int MOTOR_EN_B_PIN = 11;
 
 
 //Objects Initializations
-Servo arm;                  //for servo (both arms)
+Servo leftArm;              //for left servo
+Servo rightArm;             //for right servo
 Navigator mainAxle;         //for motor driver
 BluetoothSerial SerialBT;   //for bluetooth
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  SerialBT.begin("ESP32Number2");
-  arm.attach(SERVO_PIN);
+  Serial.begin(115200);     //for Serial monitor
+  Serial1.begin(115200);    //for UART sending message to arduino uno
+  SerialBT.begin("ESP32");
+  Serial.println("started succesfully");
+  leftArm.attach(LEFT_SERVO_PIN);
+  rightArm.attach(RIGHT_SERVO_PIN);
   mainAxle.begin(MOTOR_EN_A_PIN, MOTOR_IN_1_PIN, MOTOR_IN_2_PIN, MOTOR_IN_3_PIN, MOTOR_IN_4_PIN, MOTOR_EN_B_PIN);
 }
 
 const int maxAngle = 180;
 const int minAngle = 0;
-const int angleDelta = 0;
+const int angleDelta = 10;
 
-int servoValue;
 char action;
+char previousAction = '\0';
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Test/debug code using serial input
-  // if (Serial.available() > 0)
-  //   action = Serial.read();
+  // default letter for action
+  action = 'g';
 
   //fetching keyboard char from processing
-  action = '\0';
-
   if (SerialBT.available())
   {
     action = char(SerialBT.read());
+    if (action != previousAction)   //print only when the input recieved differs from previous
+    {
+      Serial.println(action);
+      Serial1.println(action);
+      delay(5);  
+    }
+    previousAction = action;
   }
 
   switch (action)
   {
   case 'q':
-    arm.write(0);
+    if (leftArm.read() - angleDelta <= 0)
+      leftArm.write(0);
+    else
+      leftArm.write(leftArm.read() - angleDelta);
     break;
   case 'e':
-    arm.write(180);
+    if (leftArm.read() + angleDelta >= 180)
+      leftArm.write(180);
+    else
+      leftArm.write(leftArm.read() + angleDelta);
+    break;
+  case 'i':
+    if (rightArm.read() - angleDelta <= 0)
+      rightArm.write(0);
+    else
+      rightArm.write(rightArm.read() - angleDelta);
+    break;
+  case 'p':
+    if (rightArm.read() + angleDelta >= 180)
+      rightArm.write(180);
+    else
+      rightArm.write(rightArm.read() + angleDelta);
     break;
   case 'w':
     mainAxle.forward();
@@ -74,9 +100,9 @@ void loop() {
     mainAxle.right();
     break;
   default:
-  mainAxle.stop();
+    mainAxle.stop();
     break;
   }
 
-  delay(50);
+  delay(25);
 }
